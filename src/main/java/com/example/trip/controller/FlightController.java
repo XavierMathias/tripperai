b11d583bc4592da71d9df9f6a834cd519477bad7
David Santos <dsdsantos973@gmail.com>
2024-12-05 23:57:22 -0500

    add hello method in the FlightController class and tested the API

--- src/main/java/com/example/trip/controller/FlightController.java
+++ src/main/java/com/example/trip/controller/FlightController.java
@@ -1,14 +1,16 @@
 package com.example.trip.controller;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/flight")
+@RequestMapping("/flightapi")
 public class FlightController {
 
-
+    @GetMapping("/flight-hello")
+    public String hello(){
+        return "Hello World from Flight Controller";
+    }
 
 
 
