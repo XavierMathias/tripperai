2d09fd0c6d607939aedce03c269307be9a8dfdfb
David Santos <dsdsantos973@gmail.com>
2024-12-12 00:06:32 -0500

    change AmadeusConnect from enum to class and a list of airports based on user keyword (city)

--- src/main/java/com/example/trip/controller/AmadeusConnect.java
+++ src/main/java/com/example/trip/controller/AmadeusConnect.java
@@ -1,28 +1,36 @@
 package com.example.trip.controller;
 
-
 import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.referenceData.Locations;
 import com.amadeus.resources.Location;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-public enum AmadeusConnect {
-    INSTANCE;
+@Component
+public class AmadeusConnect {
     private Amadeus amadeus;
+
     @Value("${flight.api.key}")
     private String apiKey;
+
     @Value("${flight.api.secret_key}")
     private String apiSecretKey;
-    private AmadeusConnect(){
-        this.amadeus = Amadeus
-                .builder(apiKey, apiSecretKey)
-                .build();
+
+    @PostConstruct
+    public void init() {
+        this.amadeus = Amadeus.builder(apiKey, apiSecretKey).build();
     }
 
     public Location[] location(String keyword) throws ResponseException {
-        return amadeus.referenceData.locations.get(Params.with("keyword", keyword)
+        Location[] locations = amadeus.referenceData.locations.get(Params.with("keyword", keyword)
                 .and("subType", Locations.AIRPORT));
+        if (locations.length == 0){
+            System.out.println("There's no location");
+        }
+
+        return locations;
     }
-} // end of enum
+}
--- src/main/java/com/example/trip/controller/FlightController.java
+++ src/main/java/com/example/trip/controller/FlightController.java
@@ -2,10 +2,21 @@ package com.example.trip.controller;
 
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.Location;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.amadeus.resources.Location;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/flightapi")
@@ -15,10 +26,35 @@ public class FlightController {
     public String hello(){
         return "Hello World from Flight Controller";
     }
+    private final AmadeusConnect amadeusConnect;
+    private final ObjectMapper objectMapper;
+    private StringBuilder sb = new StringBuilder();
 
-    public Location[] locations(@RequestParam(required=true) String keyword) throws ResponseException {
-        return AmadeusConnect.INSTANCE.location(keyword);
+    @Autowired
+    public FlightController(AmadeusConnect amadeusConnect, ObjectMapper objectMapper) {
+        this.amadeusConnect = amadeusConnect;
+        this.objectMapper = objectMapper;
     }
+
+    @GetMapping("/locations")
+    public String getLocations(@RequestParam String keyword) {
+
+        try {
+            // Fetch locations
+            Location[] locations = amadeusConnect.location(keyword);
+
+            ArrayList<Location> locationArray = new ArrayList<>(Arrays.asList(locations));
+            for (Location location : locationArray) {
+                sb.append(location.toString()).append("\n");
+            }
+
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"Unable to fetch locations\"}";
+        }
+    }
+
 
 
 
